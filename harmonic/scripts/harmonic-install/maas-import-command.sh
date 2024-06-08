d5f5cd090bae859bb2f6025b0b4ae53fa3d6f4b4
Alan Janis <alanjanis@icloud.com>
2024-06-08 05:51:00 +0000

    remove tests

--- harmonic/scripts/harmonic-install/maas-import-command.sh
+++ harmonic/scripts/harmonic-install/maas-import-command.sh
@@ -8,16 +8,13 @@
 # for the newly created image.
 #
 #################################################
-# shellcheck disable=SC2312
-mapfile -t FILE_ARRAY < <( jq '.builds[].files[].name' manifest.json )
-for FILE in "${FILE_ARRAY[@]}"; do
-	if [[ -e "${FILE}" && "${FILE}" == "harmonic-installer.tar.gz" ]]; then
-		  cat <<EOF
+
+if [[ -e harmonic-installer.tar.gz ]]; then
+  cat <<EOF
   MAAS IMPORT:
   Copy the image to the MAAS server then
   Import the image to MAAS with the following command:
 
   maas admin boot-resources create name='custom/harmonic' title='Harmonic cOS' architecture='amd64/generic' filetype='tgz' sha256='$(sha256sum harmonic-installer.tar.gz | cut -d ' ' -f1)' size='$(stat -c'%s' harmonic-installer.tar.gz)' content@='harmonic-installer.tar.gz'
 EOF
-	fi
-done
+fi
