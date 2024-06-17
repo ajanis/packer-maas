2d1413dd6ffae28b04a7361649fe292f05997237
Alan Janis <alanjanis@icloud.com>
2024-06-17 11:38:01 +0000

    update deploy script

--- harmonic/scripts/harmonic-install-scripts/deploy-userdata.sh
+++ harmonic/scripts/harmonic-install-scripts/deploy-userdata.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 for host in maas maaspacker; do
-for i in harmonic-installer.sh ephemeral-deploy-command.sh harmonic-user-data-download.cloud-init harmonic-user-data-writefile.cloud-init maas-import-command.sh curtin-user-data.yml 
+for i in harmonic-installer.sh ephemeral-deploy-command.sh harmonic-user-data-download.cloud-init user-data.cloud-init harmonic-user-data-writefile.cloud-init maas-import-command.sh curtin-user-data.yml 
 do              
 scp "${i}" "${host}":/opt/userdata/scripts/
 done
