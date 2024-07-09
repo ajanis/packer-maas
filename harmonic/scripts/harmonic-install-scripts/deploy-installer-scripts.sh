ebd0f7365ed4ff56d3ba83d398ca96d0bd9feaf7
Alan Janis <alanjanis@icloud.com>
2024-07-09 19:45:58 +0000

    add rsync delete

--- harmonic/scripts/harmonic-install-scripts/deploy-installer-scripts.sh
+++ harmonic/scripts/harmonic-install-scripts/deploy-installer-scripts.sh
@@ -3,11 +3,12 @@
 if [[ -f $1 ]]; then
 fileList=$1
 else
-mapfile -d '' fileList < <(find * -type f ! -name '*.tgz') || true
+mapfile -d '' fileList < <(find . -type f ! -name '*.tgz') || true
 fi
 
 # for file in "${fileList[@]}"; do     
 for host in maas maaspacker; do
 echo -e "\nUploading to ${host}:"
-scp ${fileList[@]}  "${host}:/opt/userdata/scripts/"
+rsync -azp --delete --recursive  ${fileList[@]}  "${host}":/opt/userdata/scripts/
+rsync -azp --delete --exclude '*.tgz' . "${host}":/opt/userdata/scripts/
 done
