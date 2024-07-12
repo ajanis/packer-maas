a6bbcac0761c4a8931519d1fa3e3b147c8a0d685
Alan Janis <alanjanis@icloud.com>
2024-07-12 04:23:14 +0000

    change ddeploy-installer-script to rsync

--- harmonic/scripts/harmonic-install-scripts/deploy-installer-scripts.sh
+++ harmonic/scripts/harmonic-install-scripts/deploy-installer-scripts.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
-
-if [[ -f $1 ]]; then
-fileList=$1
-else
-mapfile -d '' fileList < <(find . -type f ! -name '*.tgz') || true
-fi
-
-# for file in "${fileList[@]}"; do     
+  
 for host in maas maaspacker; do
 echo -e "\nUploading to ${host}:"
-rsync -azp --delete --recursive  ${fileList[@]}  "${host}":/opt/userdata/scripts/
-rsync -azp --delete --exclude '*.tgz' . "${host}":/opt/userdata/scripts/
+rsync -azvp --delete --exclude '*.tgz' . "${host}":/opt/userdata/scripts/
 done
